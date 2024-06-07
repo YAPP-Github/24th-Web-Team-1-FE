24cea5542c4a31b0058e007fb702587392a14217
happhee <happhee@joonggonara.co.kr>
2024-06-07 23:47:21 +0900

    docs : 필요없는 파일 삭제

--- src/article/components/articlePopup/articlePopup.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, test } from "vitest";
-
-describe("Time formatter utils", () => {
-  test("test counter format to minute and seconds", () => {
-    const expected = "00:09";
-    const count = 9;
-  });
-});
