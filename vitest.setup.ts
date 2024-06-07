fe1ae698996f81a86d9ad728081f65b82c0deef0
happhee <ghdtj7@naver.com>
2024-06-07 12:03:39 +0900

    feat : vi 모킹하는 모듈 테스트 앞뒤로 clear, reset 하도록 설정

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,8 +1,13 @@
-import { afterEach } from "vitest";
+import { afterAll, afterEach, vi } from "vitest";
 
 import "@testing-library/jest-dom";
 import { cleanup } from "@testing-library/react";
 
 afterEach(() => {
   cleanup();
+  vi.clearAllMocks();
+});
+
+afterAll(() => {
+  vi.resetAllMocks();
 });
