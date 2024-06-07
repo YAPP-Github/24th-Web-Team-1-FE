f642733842bffd1598d0115663817c982e4cd599
happhee <ghdtj7@naver.com>
2024-06-07 12:05:03 +0900

    feat : 퀴즈 풀때 버튼의 타이틀명, class속성 제어하고자 context 타입및 구현

---
+++ src/quiz/context/quizContext/index.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { createContext, useState } from "react";
+
+import { QuizContextInfo } from "@quiz/types";
+
+const QuizContext = createContext<QuizContextInfo>({
+  states: { answer: "", isSubmit: false },
+  actions: {
+    updateAnswer: () => {},
+    updateSubmit: () => {},
+  },
+});
+
+function QuizProvider({ children }: { children: React.ReactElement }) {
+  const [answer, setAnswer] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const value = {
+    states: { answer, isSubmit },
+    actions: {
+      updateAnswer: (answer: string) => setAnswer(answer),
+      updateSubmit: (isSubmit: boolean) => setIsSubmit(isSubmit),
+    },
+  };
+  return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
+}
+
+const QuizConsumer = QuizContext.Consumer;
+export { QuizConsumer, QuizProvider };
+export default QuizContext;
--- src/quiz/types/index.ts
+++ src/quiz/types/index.ts
@@ -1,1 +1,10 @@
-//type
+export type QuizContextInfo = {
+  states: {
+    answer: string | null;
+    isSubmit: boolean;
+  };
+  actions: {
+    updateAnswer: (answer: string) => void;
+    updateSubmit: (isSubmit: boolean) => void;
+  };
+};
