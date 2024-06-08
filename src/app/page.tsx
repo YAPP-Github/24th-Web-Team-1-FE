b2e7bc132db991100aa68540243eb7cdd9aafad3
soomin9106 <soomin9106@naver.com>
2024-06-09 00:41:27 +0900

    fix: temporarily remove popup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,42 +37,12 @@ export default function MainPage() {
 
 
   return (
-    <main className="flex h-[100dvh] w-full flex-col items-center overflow-hidden">
-      {
-        isClient && (
-          <SubscribePopup
-            isOpen={isOpen}
-            setIsOpen={setIsOpen}
-            title={SUBSCRIBE_POPUP_TITLE}
-            content={
-              <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-[20px]">
-                  <FormField
-                    control={form.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormControl>
-                          <Input placeholder={EMAIL_PLACEHOLDER} {...field} className={`rounded-[10px] focus-visible:ring-transparent text-[16px] ${form.formState.errors.email ? 'border-error' : ''}`} />
-                        </FormControl>
-                        <FormMessage />
-                        <span className="text-[12px] font-semibold text-text-gray2 mt-[11px]">
-                          {SUBSCRIBE_ANNOUCE.SUBSCRIBE_CONSEQUENCE} <a className="text-[12px] font-semibold underline">{SUBSCRIBE_ANNOUCE.PRIVACY_COLLECTION_NOTICE}</a>과 <a className="text-[12px] font-semibold underline">{SUBSCRIBE_ANNOUCE.PROMOTIONAL_CONSENT_NOTICE}</a>에 {SUBSCRIBE_ANNOUCE.AGREEMENT_NOTICE}
-                        </span>
-                      </FormItem>
-                    )}
-                  />
-                  <div className="flex flex-row w-full space-x-[8px]">
-                    <Button onClick={() => setIsOpen(false)} type="button" variant={"outline"} className={"bg-white text-black font-medium text-[14px] w-1/2 rounded-none"}>{SUBSCRIBE_REJECT}</Button>
-                    <Button onClick={() => setIsOpen(false)} type="submit" variant={"outline"} className={"bg-black text-white font-medium text-[14px] w-1/2 rounded-none"}>{SUBSCRIBE_ACCEPT}</Button>
-                  </div>
-                </form>
-              </Form>
-            }
-          />
-        )
-      }
+    <main className="flex w-full h-[100vh] flex-col items-center">
+      <div className="flex h-full">
+        <div>Main</div>
+      </div>
       <SubscribeBottomBar />
     </main>
+
   );
 }
