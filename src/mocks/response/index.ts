2d7bdbdb929b7ce62aae63e39f740450d685acdb
happhee <ghdtj7@naver.com>
2024-06-09 12:30:05 +0900

    feat : implementation of quiz answer api response value

--- src/mocks/response/index.ts
+++ src/mocks/response/index.ts
@@ -1,10 +1,12 @@
 import { apiRoutes } from "@shared/constants/apiRoutes";
 
 import quiz from "./quiz.json";
+import quizAnswer from "./quizAnswer.json";
 import tags from "./tags.json";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   [apiRoutes.quiz]: quiz,
   [apiRoutes.tags]: tags,
+  [apiRoutes.quizAnswer]: quizAnswer,
 };
