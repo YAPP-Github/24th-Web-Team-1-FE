78338df6eac1bc3325c9a4949eeade221aefdbea
happhee <ghdtj7@naver.com>
2024-06-09 12:29:25 +0900

    feat : implementation of quiz answer api routes

---
+++ src/app/api/quizAnswer/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+import { apiRoutes } from "@shared/constants/apiRoutes";
+
+import response from "@mocks/response";
+
+export async function GET(request: Request) {
+  try {
+    return NextResponse.json(response[apiRoutes.quizAnswer], { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
