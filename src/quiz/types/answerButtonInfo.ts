aa9f53682d3a1607747a01afc4b72daf4fe0b6f4
seohee0112 <ghdtj7@naver.com>
2024-06-10 22:10:07 +0900

    docs : rename quiz to problem

--- src/quiz/types/answerButtonInfo.ts
+++
@@ -1,8 +0,0 @@
-import { BUTTON_STATE } from "@quiz/constants/answerButtonInfo";
-
-import { ConstKeyObject } from "@common/types/constKeyObject";
-
-export type ButtonInfo = ConstKeyObject<
-  (typeof BUTTON_STATE)[number],
-  React.ButtonHTMLAttributes<HTMLButtonElement>
->;
