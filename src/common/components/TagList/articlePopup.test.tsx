6bd92d4458fd7c9d03d5de869d1ea571ff900a5a
happhee <ghdtj7@naver.com>
2024-06-10 18:06:47 +0900

    docs : Delete Unnecessary Files

--- src/common/components/TagList/articlePopup.test.tsx
+++
@@ -1,8 +0,0 @@
-import ArticlePopup from ".";
-import { render } from "@testing-library/react";
-
-describe("Time formatter utils", () => {
-  test("test counter format to minute and seconds", () => {
-    const { container } = render(<ArticlePopup />);
-  });
-});
