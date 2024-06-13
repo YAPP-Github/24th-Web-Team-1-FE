9d5f4dd6245c644c0d7f0695ead1d5ef2631c35c
happhee <ghdtj7@naver.com>
2024-06-13 14:56:11 +0900

    feat : submit answer handler and response

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -11,13 +11,30 @@ export const tagsHandler = http.get(apiRoutes.tags, () =>
   HttpResponse.json(response[apiRoutes.tags]),
 );
 
-export const problemsHandler = http.get(apiRoutes.problems, ({ request }) => {
-  const url = new URL(request.url);
-  const articleId = url.searchParams.get("articleId");
+export const problemsHandler = http.get(apiRoutes.problems, ({ params }) => {
+  const articleId = params?.articleId;
   if (!articleId) {
     return new HttpResponse(null, { status: 404 });
   }
   return HttpResponse.json(response[apiRoutes.problems]);
 });
 
-export const handlers = [quizHandler, tagsHandler, problemsHandler];
+export const submitAnswerHandler = http.post(
+  apiRoutes.submitAnswer,
+  async ({ request, params }) => {
+    const problemId = params?.problemId;
+    const result: any = await request.json();
+    const choiceAns = result?.choiceAns;
+
+    if (!choiceAns && problemId) {
+      return new HttpResponse(null, { status: 404 });
+    }
+    return HttpResponse.json(response[apiRoutes.submitAnswer]);
+  },
+);
+export const handlers = [
+  quizHandler,
+  tagsHandler,
+  problemsHandler,
+  submitAnswerHandler,
+];
--- src/mocks/response/index.ts
+++ src/mocks/response/index.ts
@@ -2,13 +2,13 @@ import { apiRoutes } from "@shared/constants/apiRoutes";
 
 import problems from "./problems.json";
 import quiz from "./quiz.json";
-import quizAnswer from "./quizAnswer.json";
+import submitAnswer from "./submitAnswer.json";
 import tags from "./tags.json";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   [apiRoutes.quiz]: quiz,
   [apiRoutes.tags]: tags,
-  [apiRoutes.quizAnswer]: quizAnswer,
   [apiRoutes.problems]: problems,
+  [apiRoutes.submitAnswer]: submitAnswer,
 };
