97bdbb4e25b5ceb71502345eee8bda999818441a
happhee <ghdtj7@naver.com>
2024-06-13 14:58:18 +0900

    fix : server error handlers

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -4,7 +4,7 @@ import "@testing-library/jest-dom";
 import { server } from "@mocks/server";
 import { cleanup } from "@testing-library/react";
 
-beforeAll(() => server.listen());
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
 
 afterEach(() => {
   cleanup();
