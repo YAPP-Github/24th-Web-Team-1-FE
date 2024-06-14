eef916f343af70913bb682f688605b539c960080
happhee <ghdtj7@naver.com>
2024-06-14 13:57:53 +0900

    fix : tag list from api data

--- src/problem/components/TagList/index.tsx
+++ src/problem/components/TagList/index.tsx
@@ -2,18 +2,27 @@
 
 import React from "react";
 
-import { useQuery } from "@tanstack/react-query";
+import { ApiResponse } from "@api/api-config";
+import queryClient from "@api/query-client";
 
 import Tag from "@common/components/Tag";
-import { getTagQueryOptions } from "@problem/remotes/getTagQueryOptions";
+import { PromblemInfo } from "@problem/types/problemInfo";
 
 export default function TagList() {
-  const { data: tagInfo, isError } = useQuery({ ...getTagQueryOptions() });
-  if (isError) return <div>error</div>;
+  const problemInfoData = queryClient.getQueryData(["get-Problems-info"]) as
+    | ApiResponse<PromblemInfo>
+    | undefined;
+
+  if (!problemInfoData) return <div>error</div>;
+
+  const { day } = problemInfoData.data;
+  const tagList = [day];
 
   return (
     <div className="mt-[4px] flex gap-[12px]">
-      {tagInfo?.tags.map((tag) => <Tag key={tag} title={tag} />)}
+      {tagList.map((tag) => (
+        <Tag key={tag} title={tag} />
+      ))}
     </div>
   );
 }
