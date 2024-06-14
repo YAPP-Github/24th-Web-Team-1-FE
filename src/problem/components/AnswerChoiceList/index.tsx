534668dffecb95b96cf95b13fe93d27eb34c8018
happhee <ghdtj7@naver.com>
2024-06-14 13:54:21 +0900

    feat : answerchoicelist component

---
+++ src/problem/components/AnswerChoiceList/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
+import React from "react";
+
+import { useQuery } from "@tanstack/react-query";
+
+import AnswerChoiceButton from "../AnswerChoiceButton";
+import { getProblemQueryOptions } from "@problem/remotes/getProblemQueryOptions";
+
+export default function AnswerChoiceList() {
+  const { problemId } = useParams<{ problemId: string }>();
+  const problemIdNumber = Number(problemId);
+  const { data: problemInfo } = useQuery({
+    ...getProblemQueryOptions({ problemId: problemIdNumber }),
+  });
+
+  if (!problemInfo) return <div>error</div>;
+
+  return (
+    <section className="mt-[39px] flex flex-col gap-[9px]">
+      {problemInfo.contents.map((answerValue) => (
+        <AnswerChoiceButton {...answerValue} key={answerValue.content} />
+      ))}
+    </section>
+  );
+}
