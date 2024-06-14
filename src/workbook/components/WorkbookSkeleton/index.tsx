ce9716e8d0a3f86e0fb240385f5a1828ce688258
soomin9106 <soomin9106@naver.com>
2024-06-14 22:46:31 +0900

    refactor: Content Skeleton Component

--- src/workbook/components/WorkbookSkeleton/index.tsx
+++ src/workbook/components/WorkbookSkeleton/index.tsx
@@ -1,19 +1,28 @@
 import { Skeleton } from "@shared/components/ui/skeleton";
 
+import ContentSkeleton from "@common/components/ContentSkeleton";
+
 export default function WorkbookSkeleton() {
+  const skeletonItems = new Array(6).fill(null);
+
   return (
     <div className="flex h-[100vh] w-full flex-col items-center overflow-x-hidden">
       <Skeleton className="h-[338px] w-full" />
 
       {/* Content Skeleton */}
-      <div className="w-full mt-[70px]">
-        <Skeleton className="h-[30px]" style={{ width: "60%" }}/>
-        <Skeleton className="h-[30px]" style={{ width: "80%", marginTop: "9px" }} />
-        <Skeleton className="h-[30px] w-full" style={{ marginTop: "9px" }} />
-        <Skeleton className="h-[30px] w-full" style={{ marginTop: "9px" }} />
-        <Skeleton className="h-[30px] w-full" style={{ marginTop: "9px" }} />
-        <Skeleton className="h-[30px] w-full" style={{ marginTop: "9px" }} />
-        <Skeleton className="h-[30px] w-full" style={{ marginTop: "9px" }} />
+      <div className="mt-[70px] w-full">
+        <ContentSkeleton className="h-[30px]" style={{ width: "60%" }} />
+        <ContentSkeleton
+          className="h-[30px]"
+          style={{ width: "80%", marginTop: "9px" }}
+        />
+        {skeletonItems.map((_, index) => (
+          <ContentSkeleton
+            key={index}
+            className="h-[30px] w-full"
+            style={{ marginTop: "9px" }}
+          />
+        ))}
       </div>
     </div>
   );
