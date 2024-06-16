58d09436aee7dfe4daf6b01a250205fad98d71b5
happhee <ghdtj7@naver.com>
2024-06-16 18:03:22 +0900

    test : click the Submit Answer button after selecting the correct answer, check whether the commentary component is well exposed

---
+++ src/app/problem/[problemId]/problemIdpage.test.tsx
@@ -0,0 +1,65 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { describe, expect, it, vi } from "vitest";
+
+import QueryClientProviders from "@shared/components/queryClientProvider";
+import { createQueryProviderWrapper } from "@shared/constants/createQueryProvider";
+
+import ProblemPage from "./page";
+import { getProblemQueryOptions } from "@problem/remotes/getProblemQueryOptions";
+import { render, renderHook, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+vi.mock("next/navigation", () => ({
+  useParams: () => ({
+    get: () => {},
+    query: {
+      problemId: "1",
+    },
+  }),
+}));
+
+describe("문제풀기 페이지 테스트", () => {
+  it("문제데이터 잘 가져오는지 확인", async () => {
+    render(
+      <QueryClientProviders>
+        <ProblemPage />
+      </QueryClientProviders>,
+    );
+
+    const { result } = renderHook(
+      () => useQuery({ ...getProblemQueryOptions({ problemId: 1 }) }),
+      { wrapper: createQueryProviderWrapper() },
+    );
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true);
+    });
+  });
+
+  it("정답 선택 이후 정답 제출하기 버튼 클릭 시, 해설 컴포넌트 잘 노출되는지 확인", async () => {
+    render(
+      <QueryClientProviders>
+        <ProblemPage />
+      </QueryClientProviders>,
+    );
+
+    const choiceAnswerButton = screen.getByRole("button", {
+      name: "높은 운용 비용",
+    });
+    await userEvent.click(choiceAnswerButton);
+
+    const answerSubmitButton = screen.getByRole("button", {
+      name: "정답 제출하기",
+    });
+    await userEvent.click(answerSubmitButton);
+
+    const problemExplanation = screen.getByRole("article");
+
+    expect(problemExplanation.childElementCount).toBe(2);
+    const explanationParagraphy = screen.getByRole("paragraph");
+
+    expect(explanationParagraphy.textContent).toBe(
+      "ETF는 일반적으로 낮은 운용 비용을 특징으로 합니다.이는 ETF가 보통 지수 추종(passive management) 방식으로 운용되기 때문입니다. 지수를 추종하는 전략은 액티브 매니지먼트(active management)에 비해 관리가 덜 복잡하고, 따라서 비용이 낮습니다.",
+    );
+  });
+});
