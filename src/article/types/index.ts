82d41f59f84d9ae4570363e657034af7833608f2
happhee <ghdtj7@naver.com>
2024-06-19 16:42:21 +0900

    feat : utilize article detail type with workbook type

--- src/article/types/index.ts
+++ src/article/types/index.ts
@@ -1,1 +1,16 @@
-//type
+import { ProblemListInfo } from "@common/types/problemListContextInfo";
+import { Article, WorkbookInfo, Writer } from "@workbook/types";
+
+type ArticleDetail = {
+  problemIds: ProblemListInfo[];
+  content: string;
+} & Article &
+  Pick<WorkbookInfo, "category" | "createdAt">;
+
+export type ArticleDetailInput = {
+  writer: Writer;
+} & ArticleDetail;
+
+export type ArticleDetailOutput = {
+  writers: Writer[];
+} & ArticleDetail;
