561d6b79ff2a379fccadb71f12eec951170ec9f1
happhee <ghdtj7@naver.com>
2024-06-19 17:19:24 +0900

    fix : get articledetail type

--- src/article/remotes/getArticleQueryOptions.ts
+++ src/article/remotes/getArticleQueryOptions.ts
@@ -3,31 +3,24 @@ import { UseQueryOptions } from "@tanstack/react-query";
 import { ApiResponse, axiosRequest } from "@api/api-config";
 
 import { API_ROUTE, QUERY_KEY } from "./api";
-import { ArticleDetailInput, ArticleDetailOutput } from "@article/types";
+import { ArticleDetail } from "@article/types";
 
 export const getArticleInfo = ({
   articleId,
-}: ArticleInfoParams): Promise<ApiResponse<ArticleDetailInput>> => {
+}: ArticleInfoParams): Promise<ApiResponse<ArticleDetail>> => {
   return axiosRequest("get", API_ROUTE.ARTICLE(articleId));
 };
 export const getArticleQueryOptions = ({
   articleId,
 }: ArticleInfoParams): UseQueryOptions<
-  ApiResponse<ArticleDetailInput>,
+  ApiResponse<ArticleDetail>,
   Error,
-  ArticleDetailOutput
+  ArticleDetail
 > => {
   return {
     queryKey: [QUERY_KEY.GET_ARTICLE, articleId],
     queryFn: () => getArticleInfo({ articleId }),
-    select: (data) => {
-      const articleInfo = data.data;
-
-      return {
-        ...articleInfo,
-        writers: [articleInfo.writer],
-      };
-    },
+    select: (data) => data.data,
   };
 };
 
