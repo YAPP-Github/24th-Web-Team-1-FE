97ec0498d19be76ab41c23376e653c82893bce9a
happhee <ghdtj7@naver.com>
2024-06-20 19:54:45 +0900

    test : link share test file

---
+++ src/common/components/LinkShareContent/LinkShareContent.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import LinkShareContent from ".";
+import userEvent from "@testing-library/user-event";
+import { COPY_CLIP_URL } from "@common/constants/linkShareContent";
+
+const href = "www.few.article.co.kr";
+
+const mockToast = vi.fn();
+vi.mock("@shared/components/ui/use-toast", () => {
+  return {
+    useToast: () => ({
+      toast: mockToast,
+    }),
+  };
+});
+
+describe("링크 공유 팝업 내부 컨텐트의 버튼 요소 테스트", () => {
+  it("href 텍스트 노출 확인", () => {
+    render(<LinkShareContent href={href} />);
+    expect(screen.getByDisplayValue(href)).toBeTruthy();
+  });
+
+  it("버튼 클릭시, 클립보드 복사 및 토스트 노출 확인", async () => {
+    render(<LinkShareContent href={href} />);
+
+    const linkClipButton = screen.getByRole("button");
+    const user = userEvent.setup();
+    await user.click(linkClipButton);
+    await waitFor(() => {
+      // 여기서 에러나는중...ㅠㅠ.ㅠ
+      expect(navigator.clipboard.writeText).toBeCalledWith(href);
+      expect(mockToast).toHaveBeenCalledWith({
+        title: COPY_CLIP_URL,
+      });
+    });
+  });
+});
