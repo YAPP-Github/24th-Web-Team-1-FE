f54077d7586d953375a3d05a83ba5dcfeb85efad
happhee <ghdtj7@naver.com>
2024-06-20 19:56:43 +0900

    feat : linkshareDialog in title section

--- src/common/components/TitleSection/index.tsx
+++ src/common/components/TitleSection/index.tsx
@@ -1,9 +1,13 @@
-import React, { ReactElement } from "react";
-
-import { Writer } from "@workbook/types";
+"use client";
+import React, { ReactElement, useState } from "react";
 
 import ShareIcon from "public/assets/icon/share.svg";
 import Tag from "../Tag";
+import ExternalControlOpenDialog from "@shared/components/ExternalControlOpenDialog";
+import { LINK_SHARE_CONTENT } from "@common/constants/linkShareContent";
+import LinkShareContent from "../LinkShareContent";
+import { usePathname } from "next/navigation";
+import LinkSharedDescription from "../LinkShareDescription";
 
 interface TitleSectionProps {
   category: string;
@@ -16,16 +20,44 @@ export default function TitleSection({
   title,
   editorComponent,
 }: TitleSectionProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  const onClickControlOpenDialog = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <header className="flex flex-col gap-[12px]">
-      <Tag title={category} />
-      <div className="flex items-center">
-        <h1 className="h1-bold text-[28px] text-black">{title}</h1>
-        <div className="ml-auto">
-          <ShareIcon width={16} height={22} />
+    <>
+      <header className="flex flex-col gap-[12px]">
+        <Tag title={category} />
+        <div className="flex items-center">
+          <h1 className="h1-bold text-[28px] text-black">{title}</h1>
+          <div className="ml-auto">
+            <ShareIcon
+              width={16}
+              height={22}
+              onClick={onClickControlOpenDialog}
+            />
+          </div>
         </div>
-      </div>
-      <section className="mt-[2px]">{editorComponent}</section>
-    </header>
+        <section className="mt-[2px]">{editorComponent}</section>
+      </header>
+      <ExternalControlOpenDialog
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        title={LINK_SHARE_CONTENT.ARTICLE_INFO.TITLE}
+        description={
+          <LinkSharedDescription
+            content={LINK_SHARE_CONTENT.ARTICLE_INFO.DESCRIPTION}
+          />
+        }
+        content={
+          <LinkShareContent
+            href={`${process.env.NEXT_PUBLIC_FEW_WEB}${pathname}`}
+          />
+        }
+      />
+    </>
   );
 }
