7af1ff379768d4d42c80d42515ca5e31b9364ebd
soomin9106 <soomin9106@naver.com>
2024-06-23 18:21:39 +0900

    feat: remove user info in query string

--- src/app/unsubscribe/page.tsx
+++ src/app/unsubscribe/page.tsx
@@ -1,13 +1,35 @@
-"use client"
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+import { useEffect } from "react";
 
 import UnsubscribeForm from "@workbook/components/UnsubscribeForm";
 import UnsubscribeTitle from "@workbook/components/UnsubscribeTitle";
 
-export default function UnsubscribePage () {
-    return (
-        <div className="flex h-full flex-col space-y-10">
-            <UnsubscribeTitle />
-            <UnsubscribeForm />
-        </div>
-    )
+export default function UnsubscribePage() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const user = searchParams.get("user");
+
+  useEffect(
+    () =>
+      function makeQueryStringInvisible() {
+        if (user) {
+          const newPath = pathname.split("?")[0];
+          router.replace(newPath);
+
+          // console.log(user);
+        }
+      },
+    [user, pathname, router],
+  );
+
+  return (
+    <div className="flex h-full flex-col space-y-10">
+      <UnsubscribeTitle />
+      <UnsubscribeForm />
+    </div>
+  );
 }
