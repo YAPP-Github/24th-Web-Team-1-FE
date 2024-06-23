ced3f19043f78f88875654a03fcf0148f07bc339
soomin9106 <soomin9106@naver.com>
2024-06-23 16:18:42 +0900

    feat: new unsubscription page

---
+++ src/app/unsubscribe/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import UnsubscribeForm from "@workbook/components/UnsubscribeForm";
+import UnsubscribeTitle from "@workbook/components/UnsubscribeTitle";
+
+export default function UnsubscribePage () {
+    return (
+        <div className="flex h-full flex-col space-y-10">
+            <UnsubscribeTitle />
+            <UnsubscribeForm />
+        </div>
+    )
+}
