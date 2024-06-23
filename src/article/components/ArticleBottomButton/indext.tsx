64285914ae3e9410dfe2e2a1418d7e429bb6d03a
happhee <ghdtj7@naver.com>
2024-06-23 15:29:11 +0900

    feat : apply the currentProblemId of the useProblemIdsViewModel to the button on the Article Page

--- src/article/components/ArticleBottomButton/indext.tsx
+++ src/article/components/ArticleBottomButton/indext.tsx
@@ -1,9 +1,22 @@
+"use client";
+
+import useProblemIdsViewModel from "@article/models/useProblemIdsViewModel";
 import { Button } from "@shared/components/ui/button";
+import { useRouter } from "next/navigation";
 import React, { HTMLAttributes } from "react";
 
 interface ArticleBottomButtonProps extends HTMLAttributes<HTMLButtonElement> {}
 export default function ArticleBottomButton({
   title,
 }: ArticleBottomButtonProps) {
-  return <Button className="w-full rounded-none bg-main">{title}</Button>;
+  const { push } = useRouter();
+  const { currentProblemId } = useProblemIdsViewModel();
+  const onClickGoProblem = () => {
+    push(`/problem/${currentProblemId}`);
+  };
+  return (
+    <Button className="w-full rounded-none bg-main" onClick={onClickGoProblem}>
+      {title}
+    </Button>
+  );
 }
