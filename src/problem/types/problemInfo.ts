1e3ffaee84855ebf68c136c491821644d6d91d2c
happhee <ghdtj7@naver.com>
2024-06-23 15:32:05 +0900

    fix : problemid comes down to number, but client changes and isolates common type to utilize string

--- src/problem/types/problemInfo.ts
+++ src/problem/types/problemInfo.ts
@@ -1,3 +1,4 @@
+import { ApiResponse } from "@api/api-config";
 import { ConstKeyObject } from "@common/types/constKeyObject";
 import {
   ANSWER_CHOICHE_TYPE,
@@ -8,17 +9,27 @@ export type ProblemTitleInfo = ConstKeyObject<
   (typeof PROBLEM_ANSWER_TYPE)[number],
   React.HTMLAttributes<HTMLHeadElement>
 >;
-
-export type PromblemInfo = {
+export type ProblemListInfo = number;
+export type ProblemParams = {
+  problemId: string;
+};
+export type PromblemServerInfo = {
   id: number;
   title: string;
   day: string;
-  contents: AnswerChoiceInfo[];
+  contents: AnswerChoiceServerInfo[];
 };
-export type AnswerChoiceInfo = {
+export type PromblemClientInfo = {
+  contents: AnswerChoiceClientInfo[];
+} & Omit<PromblemServerInfo, "contents">;
+
+export type AnswerChoiceServerInfo = {
   number: number;
   content: string;
 };
+export type AnswerChoiceClientInfo = {
+  number: string;
+} & Omit<AnswerChoiceServerInfo, "number">;
 
 export type AnswerChoiceButtonInfo = ConstKeyObject<
   (typeof ANSWER_CHOICHE_TYPE)[number],
@@ -29,4 +40,13 @@ export type AnswerCheckInfo = {
   explanation: string;
   isSolved: boolean;
   answer: string;
+};
+
+export type ProblemAnswerBody = {
+  choiceAns: string;
+};
+
+export type ProblemAnswerMuationState = {
+  data: ApiResponse<AnswerCheckInfo>;
+  variables: ProblemAnswerBody;
 };
