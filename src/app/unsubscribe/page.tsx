47d34b24d671dab34de487bbd925f017e41d505f
soomin9106 <soomin9106@naver.com>
2024-06-26 17:59:54 +0900

    feat: add url redirect logic in middleware and save email in cookie

--- src/app/unsubscribe/page.tsx
+++ src/app/unsubscribe/page.tsx
@@ -1,30 +1,9 @@
 "use client";
 
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-
-import { useEffect } from "react";
-
 import UnsubscribeForm from "@workbook/components/UnsubscribeForm";
 import UnsubscribeTitle from "@workbook/components/UnsubscribeTitle";
 
 export default function UnsubscribePage() {
-  const pathname = usePathname();
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const user = searchParams.get("user");
-
-  useEffect(
-    () =>
-      function makeQueryStringInvisible() {
-        if (user) {
-          const newPath = pathname.split("?")[0];
-          router.replace(newPath);
-
-          // console.log(user);
-        }
-      },
-    [user, pathname, router],
-  );
 
   return (
     <div className="flex h-full flex-col space-y-10">
