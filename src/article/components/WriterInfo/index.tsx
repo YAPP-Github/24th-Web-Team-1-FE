6c0e6579788eddbbe00a473e20289609790435f9
happhee <ghdtj7@naver.com>
2024-06-26 16:16:40 +0900

    fix : > next icon cover with link tag

--- src/article/components/WriterInfo/index.tsx
+++ src/article/components/WriterInfo/index.tsx
@@ -1,6 +1,7 @@
 import { Writer } from "@workbook/types";
 import React from "react";
 import NextIcon from "public/assets/icon/next.svg";
+import Link from "next/link";
 
 interface WriterInfoProps extends Writer {}
 export default function WriterInfo({ name, url }: WriterInfoProps) {
@@ -8,7 +9,9 @@ export default function WriterInfo({ name, url }: WriterInfoProps) {
     <article className="flex items-center space-x-[8px]">
       <span className="sub2-bold text-text-gray2">작가</span>
       <span className="sub2-bold text-text-gray1">{name}</span>
-      <NextIcon width={10} height={18} />
+      <Link href={url} target="_blank">
+        <NextIcon width={10} height={18} />
+      </Link>
     </article>
   );
 }
