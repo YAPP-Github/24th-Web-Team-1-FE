5199dd928c3f1fffb55afc37df6f3fdf4e44f15e
happhee <ghdtj7@naver.com>
2024-06-27 12:11:23 +0900

    style : problem layout modify

--- src/app/problem/[problemId]/page.tsx
+++ src/app/problem/[problemId]/page.tsx
@@ -1,27 +1,20 @@
 import React from "react";
 
 import AnswerChoiceList from "@problem/components/AnswerChoiceList";
-import AnswerSubmitButton from "@problem/components/AnswerSubmitButton";
 import LottieWithContext from "@problem/components/LottieWithContext";
 import ProblemExplanation from "@problem/components/ProblemExplanation";
 import ProblemTitle from "@problem/components/ProblemTitle";
+
 import TagList from "@problem/components/TagList";
-import { ProblemProvider } from "@problem/context/problemContext";
-import ProblemCompleteDialog from "@problem/components/ProblemCompleteDialog";
+
 export default function ProblemPage() {
   return (
-    <ProblemProvider>
-      <>
-        <div className="relative flex h-full flex-col">
-          <LottieWithContext />
-          <TagList />
-          <ProblemTitle />
-          <AnswerChoiceList />
-          <ProblemExplanation className="mt-[30px]" />
-        </div>
-        <AnswerSubmitButton />
-        <ProblemCompleteDialog />
-      </>
-    </ProblemProvider>
+    <div className="relative flex h-fit flex-col">
+      <LottieWithContext />
+      <TagList />
+      <ProblemTitle />
+      <AnswerChoiceList />
+      <ProblemExplanation className="my-[30px]" />
+    </div>
   );
 }
--- src/app/problem/layout.tsx
+++ src/app/problem/layout.tsx
@@ -1,4 +1,7 @@
+import AnswerSubmitButton from "@problem/components/AnswerSubmitButton";
+import ProblemCompleteDialog from "@problem/components/ProblemCompleteDialog";
 import ProblemTopbar from "@problem/components/ProblemTopbar";
+import { ProblemProvider } from "@problem/context/problemContext";
 import React, { ReactNode } from "react";
 
 interface ProblemLayoutProps {
@@ -6,9 +9,17 @@ interface ProblemLayoutProps {
 }
 export default function ProblemLayout({ children }: ProblemLayoutProps) {
   return (
-    <section className="relative mx-[20px] mb-[10px] flex h-auto w-full flex-col justify-between">
-      <ProblemTopbar />
-      {children}
-    </section>
+    <main className="flex h-auto w-full">
+      <ProblemProvider>
+        <section className="relative mx-[20px] mb-[10px] flex h-auto w-full flex-col justify-between">
+          <div className="flex flex-col">
+            <ProblemTopbar />
+            {children}
+          </div>
+          <AnswerSubmitButton />
+          <ProblemCompleteDialog />
+        </section>
+      </ProblemProvider>
+    </main>
   );
 }
