e7bf0ab0e3e657902981c6a69d07bc358ce4c595
soomin9106 <soomin9106@naver.com>
2024-06-27 23:19:34 +0900

    feat: link to article

--- src/workbook/components/CurriculumItem/index.tsx
+++ src/workbook/components/CurriculumItem/index.tsx
@@ -1,4 +1,11 @@
+'use client'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { useEffect, useState } from "react";
+
 import { CurriculumInfo } from "@workbook/types";
+import { getWorkbookId } from "@workbook/utils";
 
 interface CurriculumItemProps {
   day: number;
@@ -6,12 +13,19 @@ interface CurriculumItemProps {
 }
 
 export default function CurriculumItem({ day, item }: CurriculumItemProps) {
+  const pathname = usePathname()
+  const [workbookId, setWorkbookId] = useState<string>("")
+
+  useEffect(function getId() {
+    return setWorkbookId(getWorkbookId(pathname));
+    
+  }, [pathname])
   return (
-    <div className="flex flex-row items-center justify-start space-x-[20px] border-b p-2.5">
+    <Link className="flex flex-row items-center justify-start space-x-[20px] border-b p-2.5" href={`/article/${item.id}?workbookId=${workbookId}`}>
       <span className="text-base font-bold">Day {day}</span>
       <span className="ml-2 overflow-hidden overflow-ellipsis whitespace-nowrap text-base text-text-gray1">
         {item.title}
       </span>
-    </div>
+    </Link>
   );
 }
