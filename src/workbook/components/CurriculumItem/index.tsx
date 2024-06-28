baff1c11c63e5896b37a9b779b8486d708e7d128
happhee <ghdtj7@naver.com>
2024-06-28 19:56:22 +0900

    style : day layout to grid

--- src/workbook/components/CurriculumItem/index.tsx
+++ src/workbook/components/CurriculumItem/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -11,13 +11,16 @@ interface CurriculumItemProps {
 }
 
 export default function CurriculumItem({ day, item }: CurriculumItemProps) {
-  const pathname = usePathname()
-  const workbookId = useWorkbookId(pathname)
+  const pathname = usePathname();
+  const workbookId = useWorkbookId(pathname);
 
   return (
-    <Link className="flex flex-row items-center justify-start space-x-[20px] border-b p-2.5" href={`/article/${item.id}?workbookId=${workbookId}`}>
+    <Link
+      className="grid grid-cols-[1fr_3fr] items-center justify-start space-x-[20px] border-b p-2.5"
+      href={`/article/${item.id}?workbookId=${workbookId}`}
+    >
       <span className="text-base font-bold">Day {day}</span>
-      <span className="ml-2 overflow-hidden overflow-ellipsis whitespace-nowrap text-base text-text-gray1">
+      <span className="overflow-hidden overflow-ellipsis whitespace-nowrap text-base text-text-gray1">
         {item.title}
       </span>
     </Link>
