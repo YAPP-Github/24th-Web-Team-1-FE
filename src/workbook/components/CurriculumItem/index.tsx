be04bd64757649eec80defc6a807f8b91ffd5740
soomin9106 <soomin9106@naver.com>
2024-06-28 13:47:48 +0900

    style: apply flex nowrap to day

--- src/workbook/components/CurriculumItem/index.tsx
+++ src/workbook/components/CurriculumItem/index.tsx
@@ -8,7 +8,7 @@ interface CurriculumItemProps {
 export default function CurriculumItem({ day, item }: CurriculumItemProps) {
   return (
     <div className="flex flex-row items-center justify-start space-x-[20px] border-b p-2.5">
-      <span className="text-base font-bold">Day {day}</span>
+      <span className="text-base font-bold whitespace-nowrap">Day {day}</span>
       <span className="ml-2 overflow-hidden overflow-ellipsis whitespace-nowrap text-base text-text-gray1">
         {item.title}
       </span>
