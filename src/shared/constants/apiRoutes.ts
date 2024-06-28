7b70c16f575536986684e6e6d14450f4f4515b36
happhee <ghdtj7@naver.com>
2024-06-28 16:02:13 +0900

    feat : article with workbook api route

--- src/shared/constants/apiRoutes.ts
+++ src/shared/constants/apiRoutes.ts
@@ -3,5 +3,6 @@ export const apiRoutes = {
   submitAnswer: "/api/v1/problems/:problemId",
   workbook: "/api/v1/workbooks/:workbookId",
   article: "/api/v1/articles/:articleId",
-  unsubscribe: "/api/v1/subscriptions/all"
+  unsubscribe: "/api/v1/subscriptions/all",
+  articleWithWorkbook: "/api/v1/workbooks/:workbookId/articles/:articleId",
 };
