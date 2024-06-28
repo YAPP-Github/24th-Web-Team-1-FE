09fb68e0f33ac95a5b66bb433ee20ab18935c2ae
soomin9106 <soomin9106@naver.com>
2024-06-28 11:08:21 +0900

    fix: add 작가 in WriterInfo

--- src/workbook/components/WriterInfo/index.tsx
+++ src/workbook/components/WriterInfo/index.tsx
@@ -1,17 +1,24 @@
+import Link from "next/link";
+
 import React, { Fragment } from "react";
 
 import { WorkbookInfo } from "@workbook/types";
 
-type WriterInfoProps = Pick<WorkbookInfo,'writers'>
+type WriterInfoProps = Pick<WorkbookInfo, "writers">;
 
 export default function WriterInfo({ writers }: WriterInfoProps) {
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-row items-center space-x-[8px]">
+      <div className="flex items-center">
+        <span className="body1-medium text-text-gray2">작가</span>
+      </div>
       {writers.map((writer, idx) => (
         <Fragment key={idx}>
-          <span className="sub2-bold text-text-gray1">{writer.name}</span>
+          <Link href={writer.url} className="body1-medium text-text-gray1">
+            {writer.name}
+          </Link>
           {idx < writers.length - 1 && (
-            <span className="sub2-bold text-text-gray1"> · </span>
+            <span className="body1-medium text-text-gray1"> · </span>
           )}
         </Fragment>
       ))}
