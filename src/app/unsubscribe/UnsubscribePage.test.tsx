086fb3247862b8e172fada7b8a60433446f26158
soomin9106 <soomin9106@naver.com>
2024-06-30 22:35:02 +0900

    test: use beforeEach, beforeAll when mocking

--- src/app/unsubscribe/UnsubscribePage.test.tsx
+++ src/app/unsubscribe/UnsubscribePage.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it, vi } from "vitest";
+import { beforeAll, describe, expect, it, vi } from "vitest";
 
 import QueryClientProviders from "@shared/components/queryClientProvider";
 
@@ -6,9 +6,10 @@ import UnsubscribeLayout from "./layout";
 import UnsubscribePage from "./page";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { beforeEach } from "node:test";
 
 const push = vi.fn();
-const back = vi.fn()
+const back = vi.fn();
 
 vi.mock("next/navigation", async () => {
   const actual =
@@ -30,6 +31,31 @@ vi.mock("@subscription/hooks/useArticleInfo", () => ({
 }));
 
 describe("UnsubscribePage 동작 테스트", () => {
+  beforeAll(() => {
+    vi.mock("next/navigation", async () => {
+      const actual =
+        await vi.importActual<typeof import("next/navigation")>(
+          "next/navigation",
+        );
+      return {
+        ...actual,
+        useRouter: vi.fn(() => ({
+          push,
+          back,
+        })),
+      };
+    });
+  });
+
+  beforeEach(() => {
+    vi.mock("@subscription/hooks/useArticleInfo", () => ({
+        useArticleInfo: vi.fn().mockReturnValue({
+          articleId: "123",
+          workbookId: "456",
+        }),
+      }));
+  });
+
   it("Topbar 의 뒤로 가기를 클릭 했을 때 특정 워크북의 아티클 페이지로 이동한다", async () => {
     render(
       <QueryClientProviders>
