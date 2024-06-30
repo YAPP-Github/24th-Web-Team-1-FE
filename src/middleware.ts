20011d43a4b0b65329b699a1447b3798a8b33264
soomin9106 <soomin9106@naver.com>
2024-06-30 22:30:20 +0900

    refactor: middleware searchParams const

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
+import { UNSUB_PARAMS } from "@shared/constants/middlewareConstant";
+
 const withOutAuthList = [];
 const withAuthList = [];
 
@@ -32,16 +34,20 @@ export default function middleware(req: NextRequest) {
 
   /** unsubscribe page 진입 시 이메일, 아티클 아이디, 워크북 아이디 쿠키에 저장하는 로직 */
   if (email && articleId && workbookId) {
-    nextUrl.searchParams.delete("user");
-    nextUrl.searchParams.delete("articleId");
-    nextUrl.searchParams.delete("workbookId");
+    // UNSUB_PARAMS의 값을 배열로 변환하여 처리
+    const paramsToDelete = Object.values(UNSUB_PARAMS);
+
+    // searchParams.delete 반복적으로 호출
+    paramsToDelete.forEach((param) => {
+      nextUrl.searchParams.delete(param);
+    });
     const decodedEmail = decodeURIComponent(email);
 
     // Store the email in a cookie to pass to the page
     const response = NextResponse.redirect(nextUrl);
     response.cookies.set("user-email", decodedEmail);
-    response.cookies.set("articleId", articleId);
-    response.cookies.set("workbookId", workbookId);
+    response.cookies.set(UNSUB_PARAMS.ARTICLE_ID, articleId);
+    response.cookies.set(UNSUB_PARAMS.WORKBOOK_ID, workbookId);
 
     return response;
   }
