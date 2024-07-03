de05a80768daa79a91e16e67407d9ccb52822495
happhee <ghdtj7@naver.com>
2024-07-04 00:28:24 +0900

    feat : event constants

---
+++ src/shared/constants/mixpanel.ts
@@ -0,0 +1,6 @@
+export const EVENT_NAME = {
+  PROBLEM_APPEAR: "problem_appear",
+  WORKBOOK_APPEAR: "workbook_appear",
+  ARTICLE_APPREAR: "article_appear",
+  ARTICLE_PROBLEMBUTTON_TAPPED: "article_problemButton_tapped",
+};
