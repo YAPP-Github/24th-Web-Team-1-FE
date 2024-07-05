7660e723c8437615f6c1b341217ba16dbaab905e
happhee <ghdtj7@naver.com>
2024-07-05 19:03:26 +0900

    fix : body params type

--- src/shared/remotes/postLogMutationOptions.ts
+++ src/shared/remotes/postLogMutationOptions.ts
@@ -5,23 +5,19 @@ import { ApiResponse, axiosRequest } from "@api/api-config";
 import { LogData } from "@shared/types";
 import { API_ROUTE, QUERY_KEY } from ".";
 
-export const postLog = (
-  history: PostLogBody,
-): Promise<ApiResponse<unknown>> => {
-  return axiosRequest("post", API_ROUTE.LOGS(), history);
+export const postLog = (history: LogData): Promise<ApiResponse<unknown>> => {
+  return axiosRequest("post", API_ROUTE.LOGS(), {
+    history: JSON.stringify(history),
+  });
 };
 
 export const postLogMutationOptions = (): UseMutationOptions<
   ApiResponse<unknown>,
   Error,
-  PostLogBody
+  LogData
 > => {
   return {
     mutationKey: [QUERY_KEY.POST_LOG],
     mutationFn: (history) => postLog(history),
   };
-};
-
-type PostLogBody = {
-  history: LogData;
 };
