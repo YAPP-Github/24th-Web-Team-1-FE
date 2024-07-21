cfa72b9958c09f5389cdb248cbaf928a1366c030
happhee <ghdtj7@naver.com>
2024-07-21 20:21:08 +0900

    docs : rename import path

--- src/shared/constants/createQueryProvider.tsx
+++ src/shared/constants/createQueryProvider.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 import { QueryClientProvider } from "@tanstack/react-query";
 
-import queryClient from "@api/query-client";
+import queryClient from "@api/queryClient";
 
 export function createQueryProviderWrapper() {
   return ({ children }: { children: ReactNode }) => (
