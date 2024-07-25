4f687bba738ecdeea54958469b82832f1fa47d3f
happhee <ghdtj7@naver.com>
2024-07-25 20:47:07 +0900

    feat : skeleton ui with compound pattern

--- src/workbook/components/WorkbookSkeleton/index.tsx
+++ src/workbook/components/WorkbookSkeleton/index.tsx
@@ -1,29 +1,43 @@
 import { Skeleton } from "@shared/components/ui/skeleton";
 
-import ContentSkeleton from "@common/components/ContentSkeleton";
+const ImageSkeleton = () => {
+  return <Skeleton className="min-h-[420px] w-full" />;
+};
+const TitleSkeleton = () => {
+  return (
+    <div className="mt-[70px] flex w-full flex-col gap-3 px-[16px]">
+      <Skeleton className="h-[30px] w-[60%]" />
+      <Skeleton className="mt-[9px] h-[70px] w-[80%]" />
+      <Skeleton className="h-[28px] w-[40%]" />
+    </div>
+  );
+};
 
-export default function WorkbookSkeleton() {
+const OverviewSectionSkeleton = () => {
+  return (
+    <div className="mt-[70px] flex w-full flex-col gap-[3px] px-[16px]">
+      <Skeleton className="h-[24px] w-[26px]" />
+      <Skeleton className="h-[57px] w-full" />
+    </div>
+  );
+};
+
+const ContentWrapperkeleton = () => {
   const skeletonItems = new Array(6).fill(null);
 
   return (
-    <div className="flex h-[100vh] w-full flex-col items-center overflow-x-hidden">
-      <Skeleton className="h-[338px] w-full" />
-
-      {/* Content Skeleton */}
-      <div className="mt-[70px] w-full px-[16px]">
-        <ContentSkeleton className="h-[30px]" style={{ width: "60%" }} />
-        <ContentSkeleton
-          className="h-[30px]"
-          style={{ width: "80%", marginTop: "9px" }}
-        />
-        {skeletonItems.map((_, index) => (
-          <ContentSkeleton
-            key={index}
-            className="h-[30px] w-full"
-            style={{ marginTop: "9px" }}
-          />
-        ))}
-      </div>
+    <div className="mt-[70px] w-full px-[16px]">
+      {skeletonItems.map((_, index) => (
+        <Skeleton key={index} className="mt-[9px] h-[30px] w-full" />
+      ))}
     </div>
   );
-}
+};
+
+const WorkbookSkeleton = {
+  ImageSkeleton,
+  TitleSkeleton,
+  OverviewSectionSkeleton,
+  ContentWrapperkeleton,
+};
+export default WorkbookSkeleton;
