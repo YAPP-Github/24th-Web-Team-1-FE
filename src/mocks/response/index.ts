3850e2c40d1e607f25c186d825b9eb8120d0cae1
happhee <ghdtj7@naver.com>
2024-07-26 18:16:53 +0900

    mock : category get msw provider

--- src/mocks/response/index.ts
+++ src/mocks/response/index.ts
@@ -2,6 +2,7 @@ import { apiRoutes } from "@shared/constants/apiRoutes";
 
 import article1 from "./article1.json";
 import articleWithWorkbook1 from "./articleWithWorkbook1.json";
+import category from "./category.json";
 import problems1 from "./problems1.json";
 import problems2 from "./problems2.json";
 import problems3 from "./problems3.json";
@@ -23,4 +24,5 @@ export default {
   [apiRoutes.article + "1"]: article1,
   [apiRoutes.articleWithWorkbook + "1"]: articleWithWorkbook1,
   [apiRoutes.problemsWithArticle + "1"]: problemsWithArticleId1,
+  [apiRoutes.category]: category,
 };
