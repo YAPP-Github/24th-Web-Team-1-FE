e6c450e02be70220afb507a2aab33a78e119f3b1
happhee <ghdtj7@naver.com>
2024-07-26 18:25:41 +0900

    feat : main page with workbooks, articles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import ArticleCardsWrapper from "@main/components/ArticleCardsWrapper";
 import MainHeader from "@main/components/MainHeader";
+import WorkbookCardsWrapper from "@main/components/WorkbookCardsWrapper";
 
 export default function MainPage() {
   return (
-    <>
+    <main className="flex h-auto w-full flex-col">
       <MainHeader />
-    </>
+      <WorkbookCardsWrapper />
+      <ArticleCardsWrapper />
+    </main>
   );
 }
