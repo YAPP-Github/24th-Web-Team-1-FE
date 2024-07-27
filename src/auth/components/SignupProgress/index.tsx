e525aedada0115346788f0d2e1a39d2489f8f27e
soomin9106 <soomin9106@naver.com>
2024-07-27 17:05:17 +0900

    refactor: naming modification

--- src/auth/components/SignupProgress/index.tsx
+++ src/auth/components/SignupProgress/index.tsx
@@ -5,15 +5,15 @@ export default function SignupProgress() {
     <section className="flex flex-col items-center gap-y-[83px]">
       <div className="flex flex-col items-center gap-y-[10px]">
         <div className="flex flex-col items-center">
-          <h3 className="h3-bold">{SIGNUP_PROGRESS.EMAIL_SENT_1}</h3>
-          <h3 className="h3-bold">{SIGNUP_PROGRESS.EMAIL_SENT_2}</h3>
+          <h3 className="h3-bold">{SIGNUP_PROGRESS.EMAIL_SENT_EMAIL}</h3>
+          <h3 className="h3-bold">{SIGNUP_PROGRESS.EMAIL_SENT_LINK}</h3>
         </div>
         <span className="body3-medium text-text-gray1">{SIGNUP_PROGRESS.COMPLETE_MESSAGE}</span>
       </div>
       <div className="bg-background1 rounded-[4px] flex flex-col gap-y-[4px] items-start p-[15px] w-full">
         <span className="text-main text-[12px] font-bold">{SIGNUP_PROGRESS.NOT_SENT_INFO_TITLE}</span>
-        <span className="text-text-gray1 text-[12px] font-bold">{SIGNUP_PROGRESS.NOT_SENT_INFO_1}</span>
-        <span className="text-text-gray1 text-[12px] font-bold">{SIGNUP_PROGRESS.NOT_SENT_INFO_2}</span>
+        <span className="text-text-gray1 text-[12px] font-bold">{SIGNUP_PROGRESS.NOT_SENT_INFO_SPAM_CHECK}</span>
+        <span className="text-text-gray1 text-[12px] font-bold">{SIGNUP_PROGRESS.NOT_SENT_INFO_CHECK}</span>
       </div>
     </section>
   );
--- src/auth/constants/auth.ts
+++ src/auth/constants/auth.ts
@@ -9,12 +9,12 @@ export const LOGIN_OR_SIGNUP = "로그인 또는 회원가입"
 export const INTRO = "매일 아침 8시, 아티클과 3개의 문제를 이메일로 보내드려요."
 
 export const SIGNUP_PROGRESS = {
-    EMAIL_SENT_1: "입력해주신 이메일로",
-    EMAIL_SENT_2: "인증 링크를 보내드렸어요!",
+    EMAIL_SENT_EMAIL: "입력해주신 이메일로",
+    EMAIL_SENT_LINK: "인증 링크를 보내드렸어요!",
     COMPLETE_MESSAGE: "메일함에서 링크를 누르면 가입이 완료됩니다.",
     NOT_SENT_INFO_TITLE: "(i) 이메일이 오지 않는다면?",
-    NOT_SENT_INFO_1: "스팸함을 확인해주세요!",
-    NOT_SENT_INFO_2: "이메일의 철자를 제대로 입력했는지 확인해주세요!",
+    NOT_SENT_INFO_SPAM_CHECK: "스팸함을 확인해주세요!",
+    NOT_SENT_INFO_CHECK: "이메일의 철자를 제대로 입력했는지 확인해주세요!",
 }
 
 export const SIGNUP_COMPLETED = {
