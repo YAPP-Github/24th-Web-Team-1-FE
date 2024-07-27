4a05d3034116e37776426b87adf8febd69c1f6b1
soomin9106 <soomin9106@naver.com>
2024-07-27 23:26:05 +0900

    refactor: using map in h3 tag

--- src/auth/components/SignupProgress/index.tsx
+++ src/auth/components/SignupProgress/index.tsx
@@ -2,26 +2,42 @@ import { cn } from "@shared/utils/cn";
 
 import { DESCRIPTION_STYLE, SIGNUP_PROGRESS } from "@auth/constants/auth";
 
+const signupProgressMessages = [
+  SIGNUP_PROGRESS.EMAIL_SENT_EMAIL,
+  SIGNUP_PROGRESS.EMAIL_SENT_LINK,
+];
+
 export default function SignupProgress() {
   return (
     <section className="flex flex-col items-center gap-y-[83px]">
       <div className="flex flex-col items-center gap-y-[10px]">
         <div className="flex flex-col items-center">
-          <h3 className="h3-bold">{SIGNUP_PROGRESS.EMAIL_SENT_EMAIL}</h3>
-          <h3 className="h3-bold">{SIGNUP_PROGRESS.EMAIL_SENT_LINK}</h3>
+          {signupProgressMessages.map((message, index) => (
+            <h3 key={`${message}-${index}`} className="h3-bold">
+              {message}
+            </h3>
+          ))}
         </div>
-        <span className="body3-medium text-text-gray1">{SIGNUP_PROGRESS.COMPLETE_MESSAGE}</span>
+        <span className="body3-medium text-text-gray1">
+          {SIGNUP_PROGRESS.COMPLETE_MESSAGE}
+        </span>
       </div>
-      <div className={
-        cn(
+      <div
+        className={cn(
           "flex flex-col gap-y-[4px]",
-          "bg-background1 rounded-[4px]",
-          "items-start p-[15px] w-full"
-        )
-      }>
-        <span className={`text-main ${DESCRIPTION_STYLE}`}>{SIGNUP_PROGRESS.NOT_SENT_INFO_TITLE}</span>
-        <span className={`text-text-gray1 ${DESCRIPTION_STYLE}`}>{SIGNUP_PROGRESS.NOT_SENT_INFO_SPAM_CHECK}</span>
-        <span className={`text-text-gray1 ${DESCRIPTION_STYLE}`}>{SIGNUP_PROGRESS.NOT_SENT_INFO_CHECK}</span>
+          "rounded-[4px] bg-background1",
+          "w-full items-start p-[15px]",
+        )}
+      >
+        <span className={`text-main ${DESCRIPTION_STYLE}`}>
+          {SIGNUP_PROGRESS.NOT_SENT_INFO_TITLE}
+        </span>
+        <span className={`text-text-gray1 ${DESCRIPTION_STYLE}`}>
+          {SIGNUP_PROGRESS.NOT_SENT_INFO_SPAM_CHECK}
+        </span>
+        <span className={`text-text-gray1 ${DESCRIPTION_STYLE}`}>
+          {SIGNUP_PROGRESS.NOT_SENT_INFO_CHECK}
+        </span>
       </div>
     </section>
   );
