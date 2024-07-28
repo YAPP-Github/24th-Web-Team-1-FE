b026e7181845bc3efb40d971459358e2a4594eff
happhee <ghdtj7@naver.com>
2024-07-28 16:31:12 +0900

    bugfix : category response data type

--- src/common/types/category.ts
+++ src/common/types/category.ts
@@ -2,3 +2,7 @@ export type CategoryInfo = {
   code: number;
   name: string;
 };
+
+export type CategoryInfoList = {
+  categories: CategoryInfo[];
+};
--- src/main/remotes/getWorkbookCategoryQueryOptions.ts
+++ src/main/remotes/getWorkbookCategoryQueryOptions.ts
@@ -1,20 +1,20 @@
 import { ApiResponse, fewFetch } from "@api/fewFetch";
-import { CategoryInfo } from "@common/types/category";
+import { CategoryInfo, CategoryInfoList } from "@common/types/category";
 import { UseQueryOptions } from "@tanstack/react-query";
 import { API_ROUTE, QUERY_KEY } from ".";
 
-const getWorkbookCategory = (): Promise<ApiResponse<CategoryInfo[]>> => {
+const getWorkbookCategory = (): Promise<ApiResponse<CategoryInfoList>> => {
   return fewFetch().get(API_ROUTE.CATEGORY);
 };
 
 export const getWorkbookCategoryQueryOptions = (): UseQueryOptions<
-  ApiResponse<CategoryInfo[]>,
+  ApiResponse<CategoryInfoList>,
   unknown,
   CategoryInfo[]
 > => {
   return {
     queryKey: [QUERY_KEY.GET_CATEGORY],
     queryFn: () => getWorkbookCategory(),
-    select: (data) => data.data.data,
+    select: (data) => data.data.data.categories,
   };
 };
