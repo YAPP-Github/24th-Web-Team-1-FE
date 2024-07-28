bee60100c65b650d0dce27059c6607655b9e6c78
happhee <ghdtj7@naver.com>
2024-07-28 20:37:11 +0900

    style : modify main page component

--- src/main/components/ArticleCardsWrapper/index.tsx
+++ src/main/components/ArticleCardsWrapper/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import useCategory from "@main/hooks/useCategory";
+import ArticleCardList from "../ArticleCardList";
 import CategoryTabs from "../CategoryTabs";
 import MainContentWrapper from "../MainContentWrapper";
 
@@ -7,12 +8,14 @@ export default function ArticleCardsWrapper() {
   const { category, handleCategory } = useCategory();
 
   return (
-    <MainContentWrapper title="Articles" className="mt-[16px]">
+    <MainContentWrapper title="Articles" className="m-[0]">
       <CategoryTabs
         type="ARTICLE"
         handleCategory={handleCategory}
         category={category}
+        className="ml-[20px]"
       />
+      {category && <ArticleCardList {...category} />}
     </MainContentWrapper>
   );
 }
