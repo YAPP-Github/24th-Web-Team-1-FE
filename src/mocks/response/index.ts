5ba7ea71d421bbd378b2aa707bb4874a47063b2c
happhee <ghdtj7@naver.com>
2024-08-01 12:32:40 +0900

    feat : mock api response workbook subscriptions

--- src/mocks/response/index.ts
+++ src/mocks/response/index.ts
@@ -12,6 +12,7 @@ import submitAnswer1 from "./submitAnswer1.json";
 import submitAnswer2 from "./submitAnswer2.json";
 import submitAnswer3 from "./submitAnswer3.json";
 import workbook from "./workbook.json";
+import workbooksSubscription from "./workbooksSubscription.json";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
@@ -23,6 +24,7 @@ export default {
   [apiRoutes.submitAnswer + "3"]: submitAnswer3,
   [apiRoutes.workbook]: workbook,
   [apiRoutes.workbooks + "entire"]: mainWorkbooksEntire,
+  [apiRoutes.workbooksSubscription]: workbooksSubscription,
   [apiRoutes.article + "1"]: article1,
   [apiRoutes.articleWithWorkbook + "1"]: articleWithWorkbook1,
   [apiRoutes.problemsWithArticle + "1"]: problemsWithArticleId1,
