5be5bff5cafe0303a1c2c3299b3ec889e1f6d688
happhee <ghdtj7@naver.com>
2024-08-01 12:29:22 +0900

    feat : workbooks with subscription route path

--- src/shared/constants/apiRoutes.ts
+++ src/shared/constants/apiRoutes.ts
@@ -3,6 +3,7 @@ export const apiRoutes = {
   submitAnswer: `${process.env.NEXT_PUBLIC_FEW_WEB}/api/v1/problems/:problemId`,
   workbook: `${process.env.NEXT_PUBLIC_FEW_WEB}/api/v1/workbooks/:workbookId`,
   workbooks: `${process.env.NEXT_PUBLIC_FEW_WEB}/api/v1/workbooks`,
+  workbooksSubscription: `${process.env.NEXT_PUBLIC_FEW_WEB}/api/v1/subscriptions/workbooks`,
   article: `${process.env.NEXT_PUBLIC_FEW_WEB}/api/v1/articles/:articleId`,
   unsubscribe: `${process.env.NEXT_PUBLIC_FEW_WEB}/api/v1/subscriptions/all`,
   articleWithWorkbook: `${process.env.NEXT_PUBLIC_FEW_WEB}/api/v1/workbooks/:workbookId/articles/:articleId`,
