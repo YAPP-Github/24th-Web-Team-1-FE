5599d8ed65c28c698c03792437b7be92de169817
happhee <ghdtj7@naver.com>
2024-08-02 19:03:32 +0900

    feat : category field with category model

--- src/main/remotes/getWorkbookCategoryQueryOptions.ts
+++ src/main/remotes/getWorkbookCategoryQueryOptions.ts
@@ -1,20 +1,31 @@
 import { ApiResponse, fewFetch } from "@api/fewFetch";
-import { CategoryInfo, CategoryInfoList } from "@common/types/category";
+import { CategoryClientInfo, CategoryServerInfo } from "@common/types/category";
+import { CategoryModel } from "@main/models/CategoryModel";
 import { UseQueryOptions } from "@tanstack/react-query";
 import { API_ROUTE, QUERY_KEY } from ".";
 
-const getWorkbookCategory = (): Promise<ApiResponse<CategoryInfoList>> => {
+const getWorkbookCategory = (): Promise<ApiResponse<CategoryListRes>> => {
   return fewFetch().get(API_ROUTE.CATEGORY);
 };
 
 export const getWorkbookCategoryQueryOptions = (): UseQueryOptions<
-  ApiResponse<CategoryInfoList>,
+  ApiResponse<CategoryListRes>,
   unknown,
-  CategoryInfo[]
+  CategoryClientInfo[]
 > => {
   return {
     queryKey: [QUERY_KEY.GET_CATEGORY],
     queryFn: () => getWorkbookCategory(),
-    select: (data) => data.data.data.categories,
+    select: (data) => {
+      const categories = data.data.data.categories;
+      const categoryModel = new CategoryModel({
+        initCategoryServerInfoList: categories,
+      });
+      return categoryModel.CategoryClientInfoList;
+    },
   };
+};
+
+type CategoryListRes = {
+  categories: CategoryServerInfo[];
 };
