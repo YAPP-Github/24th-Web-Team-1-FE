543f4830fb6e5157d11bf627de05c3b53e348b15
soomin9106 <soomin9106@naver.com>
2024-08-05 20:03:34 +0900

    fix: remove padding

--- src/app/workbook/[id]/layout.tsx
+++ src/app/workbook/[id]/layout.tsx
@@ -7,7 +7,6 @@ import queryClient from "@api/queryClient";
 import { getWorkbookQueryOptions } from "@workbook/remotes/getWorkbookQueryOptions";
 import TopBar from "@shared/components/TopBar";
 
-
 export async function generateMetadata({
   params,
 }: {
@@ -34,10 +33,12 @@ interface WorkbookLayoutProps {
 export default function WorkbookLayout({ children }: WorkbookLayoutProps) {
   return (
     <section className="flex h-auto w-full flex-col justify-between">
-      <div className="mx-[20px] mb-[10px] flex flex-col">
+      <div className="mb-[10px] flex flex-col">
+        <div className="mx-[20px]">
           <TopBar />
-          {children}
         </div>
+        {children}
+      </div>
     </section>
-  )
+  );
 }
