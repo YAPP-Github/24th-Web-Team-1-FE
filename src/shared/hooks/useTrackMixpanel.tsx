eaad532cd001d9706225e1834b3e9e8309579d80
seohee0112 <ghdtj7@naver.com>
2024-08-09 00:11:36 +0900

    test : identity

--- src/shared/hooks/useTrackMixpanel.tsx
+++ src/shared/hooks/useTrackMixpanel.tsx
@@ -16,6 +16,7 @@ export default function useTrackMixpanel({
 }: TrackMixpanelProps) {
   useEffect(
     function trackMixpanel() {
+      Mixpanel.identify({ id: "hee" });
       Mixpanel.people.set({ peoples: { $email: "happgee" } });
       if (property)
         Mixpanel.track({
