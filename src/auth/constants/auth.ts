820f9f0f70b3baff36beaad4061d315e7ceb0a09
soomin9106 <soomin9106@naver.com>
2024-08-09 23:16:07 +0900

    feature: loading bar before receiving email

--- src/auth/constants/auth.ts
+++ src/auth/constants/auth.ts
@@ -15,7 +15,13 @@ export const SIGNUP_PROGRESS = {
     NOT_SENT_INFO_TITLE: "(i) 이메일이 오지 않는다면?",
     NOT_SENT_INFO_SPAM_CHECK: "스팸함을 확인해주세요!",
     NOT_SENT_INFO_CHECK: "이메일의 철자를 제대로 입력했는지 확인해주세요!",
-    EMAIL_SEND_FAIL: "이메일 전송에 실패했습니다."
+    EMAIL_SEND_FAIL: "이메일 전송에 실패했어요. 다시 시도해주세요."
+}
+
+export const SIGNUP_PENDING = {
+    EMAIL_TYPED: "입력해주신 이메일로",
+    EMAIL_SENDING: "인증 링크를 전송중이에요!",
+    EMAIL_PENDING_TIME: "최대 10초 정도 소요됩니다."
 }
 
 export const SIGNUP_COMPLETED = {
