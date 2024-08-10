1c0dced225dcdfb2290f183d095797f206ea047f
happhee <ghdtj7@naver.com>
2024-08-10 10:01:23 +0900

    feat : Imageloader

--- src/main/components/WorkbookCardDetail/index.tsx
+++ src/main/components/WorkbookCardDetail/index.tsx
@@ -1,9 +1,12 @@
 import { WorkbookClientInfo } from "@main/types/workbook";
 import { Button } from "@shared/components/ui/button";
 import { cn } from "@shared/utils/cn";
-import Image from "next/image";
+import Image, { ImageLoaderProps } from "next/image";
 import FewLogo from "public/assets/icon/cardFewLogo.svg";
 
+const ImageLoadr = ({ src, width, quality }: ImageLoaderProps) => {
+  return `${src}?w=${width}&q=${quality || 75}`;
+};
 const ImageWrapper = ({
   children,
 }: Readonly<{
@@ -18,6 +21,7 @@ const MainImage = ({
     height={172}
     src={mainImageUrl}
     alt="main-image"
+    loader={ImageLoadr}
     priority
     className="h-[172px] w-[269px] rounded-t-lg object-cover"
   />
