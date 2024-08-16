7e7e03003e0993bf38bcf0760c89697941fc9098
soomin9106 <soomin9106@naver.com>
2024-08-16 21:47:07 +0900

    feat: workbook study button when user subscribes workbook

--- src/workbook/components/WorkbookButton/index.tsx
+++ src/workbook/components/WorkbookButton/index.tsx
@@ -1,5 +1,7 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
+import { useQueries } from "@tanstack/react-query";
+
 import queryClient from "@api/queryClient";
 
 import useIsLogin from "@shared/hooks/useIsLogin";
@@ -7,7 +9,11 @@ import useIsLogin from "@shared/hooks/useIsLogin";
 import { WORKBOOK_BTNS } from "@workbook/constants/buttons";
 import { getWorkbookId } from "@workbook/utils";
 
+import { ENTIRE_CATEGORY } from "@main/constants";
+import { WorkbookCardModel } from "@main/models/WorkbookCardModel";
 import { QUERY_KEY } from "@main/remotes";
+import { getSubscriptionWorkbooksQueryOptions } from "@main/remotes/getSubscriptionWorkbooksQueryOptions";
+import { getWorkbooksWithCategoryQueryOptions } from "@main/remotes/getWorkbooksWithCategoryQueryOptions";
 
 import WorkbookButtonItem from "../\bWorkbookButtonItem";
 import useSusbscribeWorkbook from "@common/hooks/useSusbscribeWorkbook";
@@ -20,7 +26,40 @@ export default function WorkbookButton() {
   const pathname = usePathname();
   const workbookId = getWorkbookId(pathname);
 
-  const handleLoginClick = () => {
+  const workbookInfo = useQueries({
+    queries: [
+      getWorkbooksWithCategoryQueryOptions({
+        code: ENTIRE_CATEGORY,
+      }),
+      {
+        ...getSubscriptionWorkbooksQueryOptions(),
+        enabled: isLogin === true,
+      },
+    ],
+    combine: (result) => {
+      const [workbookServerList, workbookSubscriptionInfoList] = result;
+
+      if (workbookServerList.data) {
+        const workbookCardModel = new WorkbookCardModel({
+          initWorkbookSeverList: workbookServerList.data,
+          initWorkbookSubscriptionInfoList: workbookSubscriptionInfoList.data,
+        });
+
+        // 특정 워크북 ID에 따른 필터링 적용
+        const targetWorkbookId = Number(workbookId);
+        const filteredWorkbookCombineList =
+          workbookCardModel.workbookCombineListData.filter(
+            (workbook) => workbook.id === targetWorkbookId,
+          );
+
+        return workbookCardModel.workbookCardList({
+          workbookCombineList: filteredWorkbookCombineList,
+        });
+      }
+    },
+  });
+
+  const handleLoginSubClick = () => {
     postSubscribeWorkbook({
       workbookId: workbookId.toString(),
       handleSucess: () => {
@@ -38,13 +77,30 @@ export default function WorkbookButton() {
     router.push("/auth");
   };
 
+  const handleStudy = () => {
+    if (workbookInfo) {
+      router.push(`/article/${workbookInfo[0].articleId}?workbookId=${workbookId}`)
+    }
+  }
+
+  console.log("workbookInfo ", workbookInfo);
+
   return (
     <section className="px-[20px] pb-[44px]">
-      {isLogin ? (
-        <WorkbookButtonItem
-          title={WORKBOOK_BTNS.SUB}
-          handleClick={handleLoginClick}
-        />
+      {isLogin && workbookInfo ? (
+        <>
+          {workbookInfo[0].cardType === "SUBSCRIBE" ? (
+            <WorkbookButtonItem
+              title={WORKBOOK_BTNS.SUB}
+              handleClick={handleLoginSubClick}
+            />
+          ) : (
+            <WorkbookButtonItem
+              title={WORKBOOK_BTNS.STUDY}
+              handleClick={handleStudy}
+            />
+          )}
+        </>
       ) : (
         <WorkbookButtonItem
           title={WORKBOOK_BTNS.LOGIN_AND_SUB}
--- src/workbook/constants/buttons.ts
+++ src/workbook/constants/buttons.ts
@@ -1,4 +1,5 @@
 export const WORKBOOK_BTNS = {
     LOGIN_AND_SUB: "로그인하고 학습 시작하기",
-    SUB: "구독하기"
+    SUB: "구독하기",
+    STUDY: "학습하기",
 }
