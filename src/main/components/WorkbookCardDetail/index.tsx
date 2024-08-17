8cccb4422f1db62471be06813bd0d43660a91d95
happhee <ghdtj7@naver.com>
2024-08-17 22:21:08 +0900

    docs : remove priority

--- src/main/components/WorkbookCardDetail/index.tsx
+++ src/main/components/WorkbookCardDetail/index.tsx
@@ -23,8 +23,7 @@ const MainImage = ({
     src={mainImageUrl}
     alt="main-image"
     loader={ImageLoadr}
-    sizes="269px"
-    priority={isPriorityImage}
+    loading="lazy"
     className="h-[172px] w-[269px] rounded-t-lg object-cover"
   />
 );
