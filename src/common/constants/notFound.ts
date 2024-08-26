36590f9005d311357c06a677c65ae40c5049e08c
soomin9106 <soomin9106@naver.com>
2024-08-26 22:36:45 +0900

    feat: 404 not found page

---
+++ src/common/constants/notFound.ts
@@ -0,0 +1,9 @@
+export const NOT_FOUND_WORDS = {
+    TITLE: "죄송합니다. 페이지를 찾을 수 없습니다.",
+    WORDS_1: "존재하지 않는 주소를 입력하셨거나",
+    WORDS_2: "요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다."
+}
+
+export const wordStyle = "text-text-gray1 text-sm font-medium"
+
+export const GO_TO_MAIN = "메인으로 돌아가기"
