f316aa32d761209b532094e1146bb15c74218a60
soomin9106 <soomin9106@naver.com>
2024-08-29 19:56:38 +0900

    fix: remove console log

--- src/problem/components/ArticleDropDown/index.tsx
+++ src/problem/components/ArticleDropDown/index.tsx
@@ -6,8 +6,6 @@ import ProblemArticleTemplate from "../ProblemArticleTemplate";
 export function ArticleDropDown() {
   const { getArticleId } = useProblemIdsViewModel();
 
-  console.log(getArticleId());
-  
   return (
     <div className="absolute left-0 top-[66px] z-20 h-screen w-full bg-white overflow-y-scroll px-[20px] pb-[100px]">
       <ProblemArticleTemplate articleId={getArticleId()} />
