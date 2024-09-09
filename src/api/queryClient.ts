45d811ac7a8087e626994b764bc275f4d89dd57c
happhee <ghdtj7@naver.com>
2024-09-09 13:33:09 +0900

    fix : query client time

--- src/api/queryClient.ts
+++ src/api/queryClient.ts
@@ -3,10 +3,9 @@ import { QueryClient } from "@tanstack/react-query";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-    refetchOnMount: false,
       refetchOnWindowFocus: false,
       retry: 0,
-      staleTime: 60 * 1000,
+      // staleTime: 60 * 1000,
     },
   },
 });
